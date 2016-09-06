74259202af439273506b06ed4cdd13814c56aed5
Abe Vos <abe_vos@msn.com>
2016-09-06 22:32:52 +0200

    Add tutorial 1 prep

---
+++ Assets/Tutorial_00/Prep/Scripts/CubeTrick.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeTrick : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.05f;
+
+    [SerializeField]
+    private Color[] colorArray = { Color.red, Color.green, Color.blue };
+
+    private Renderer renderer;
+
+    void Awake ()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += 0.5f * Time.deltaTime * Vector3.up;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+           renderer.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        else if (Input.GetKey(KeyCode.B))
+        {
+            renderer.material.color = Color.black;
+        }
+    }
+}
