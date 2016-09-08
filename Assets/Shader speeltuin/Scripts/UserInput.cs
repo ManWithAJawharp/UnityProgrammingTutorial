4ea72566cb98b2f4b1d2c024fea674ca4d5be406
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-08 17:52:07 +0200

    Add scenery group and corresponding toggle key

--- Assets/Shader speeltuin/Scripts/UserInput.cs
+++ Assets/Shader speeltuin/Scripts/UserInput.cs
@@ -1,24 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class UserInput : MonoBehaviour
 {
     [SerializeField]
     private KeyCode lightKey;
+    [SerializeField]
+    private KeyCode sceneryKey;
 
     [SerializeField]
     private GameObject lightGroup;
+    [SerializeField]
+    private GameObject sceneryGroup;
 
-	void Start ()
-    {
-	
-	}
-	
 	void Update ()
     {
 	    if(Input.GetKeyUp(lightKey))
         {
             lightGroup.SetActive(!lightGroup.activeInHierarchy);
         }
-	}
+
+        if (Input.GetKeyUp(sceneryKey))
+        {
+            sceneryGroup.SetActive(!sceneryGroup.activeInHierarchy);
+        }
+    }
 }
