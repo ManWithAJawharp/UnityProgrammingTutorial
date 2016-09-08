c571290709f2afd9d56b0ebced1e552d76228192
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-08 19:51:26 +0200

    Change line endings to unicode

--- Assets/Shader speeltuin/Scripts/UserInput.cs
+++ Assets/Shader speeltuin/Scripts/UserInput.cs
@@ -1,27 +1,1 @@
-﻿using UnityEngine;
-
-public class UserInput : MonoBehaviour
-{
-    [SerializeField]
-    private KeyCode lightKey;
-    [SerializeField]
-    private KeyCode sceneryKey;
-
-    [SerializeField]
-    private GameObject lightGroup;
-    [SerializeField]
-    private GameObject sceneryGroup;
-
-	void Update ()
-    {
-	    if(Input.GetKeyUp(lightKey))
-        {
-            lightGroup.SetActive(!lightGroup.activeInHierarchy);
-        }
-
-        if (Input.GetKeyUp(sceneryKey))
-        {
-            sceneryGroup.SetActive(!sceneryGroup.activeInHierarchy);
-        }
-    }
-}
+﻿using UnityEngine;  public class UserInput : MonoBehaviour {     [SerializeField]     private KeyCode lightKey;     [SerializeField]     private KeyCode sceneryKey;      [SerializeField]     private GameObject lightGroup;     [SerializeField]     private GameObject sceneryGroup;  	void Update ()     { 	    if(Input.GetKeyUp(lightKey))         {             lightGroup.SetActive(!lightGroup.activeInHierarchy);         }          if (Input.GetKeyUp(sceneryKey))         {             sceneryGroup.SetActive(!sceneryGroup.activeInHierarchy);         }     } } 
