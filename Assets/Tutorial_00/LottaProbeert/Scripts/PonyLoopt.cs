cf2e8cb339c4f224fe7f260a4aa5be9ae3e6b08d
Lotta Unicorns <lotta.andriesse@gmail.com>
2016-09-08 19:37:46 +0200

    mn ponie loopt

---
+++ Assets/Tutorial_00/LottaProbeert/Scripts/PonyLoopt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PonyLoopt : MonoBehaviour {
+
+    public float speedForward;
+    public int heightJump;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0f, 0f, speedForward * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(0f, heightJump * Time.deltaTime, -5f);
+        }
+    }
+
+    /*    float NaarVoren (float speedForward)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                transform.Translate(0f,1f*Time.deltaTime, 0f);
+            }
+        }*/
+}
--- Assets/Tutorial_00/Prep/Scripts/LottaLeert.cs
+++ Assets/Tutorial_00/Prep/Scripts/LottaLeert.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeTrick : MonoBehaviour
+public class LottaLeert : MonoBehaviour
 {
     [SerializeField]
 
