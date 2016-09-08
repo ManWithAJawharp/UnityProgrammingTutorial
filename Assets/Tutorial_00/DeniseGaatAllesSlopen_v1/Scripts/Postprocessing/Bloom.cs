72171e2798dba794c6a0a6657d07ee11d76f53c0
Denise <d.feeleus@gmail.com>
2016-09-08 22:17:07 +0200

    Add bloom script to camera

---
+++ Assets/Tutorial_00/DeniseGaatAllesSlopen_v1/Scripts/Postprocessing/Bloom.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ArgiaIluna
+{
+    namespace PostProcessing
+    {
+        [ExecuteInEditMode]
+        public class Bloom : MonoBehaviour
+        {
+            [SerializeField]
+            private float radius = 1f;
+            [SerializeField]
+            private float threshold = 1f;
+            [SerializeField]
+            private float blurScale = 1f;
+
+            [SerializeField]
+            private float intensity = 1f;
+
+            private Material material;
+            
+            private int iterations;
+            private const int maxIterations = 8;
+
+            RenderTexture[] blurBuffer1 = new RenderTexture[maxIterations];
+            RenderTexture[] blurBuffer2 = new RenderTexture[maxIterations];
+
+            void OnEnable ()
+            {
+                material = new Material(Shader.Find("Hidden/ArgiaIluna/Bloom Shader"));
+            }
+
+            void OnRenderImage (RenderTexture source, RenderTexture destination)
+            {
+                material.SetFloat("_Threshold", threshold);
+                material.SetFloat("_BlurScale", blurScale);
+                material.SetFloat("_Intensity", intensity);
+
+                RenderTexture prefiltered = RenderTexture.GetTemporary(source.width / 2, source.height / 2, 0, RenderTextureFormat.DefaultHDR);
+
+                Graphics.Blit(source, prefiltered, material, 0);
+
+                RenderTexture previousTexture = prefiltered;
+
+                int logh = (int)(Mathf.Log(source.height, 2) + radius - 8);
+                iterations = Mathf.Clamp(logh, 1, maxIterations);
+
+                //  Downsample
+                for (int i = 0; i < iterations; i++)
+                {
+                    blurBuffer1[i] = RenderTexture.GetTemporary(previousTexture.width / 2, previousTexture.height / 2, 0, RenderTextureFormat.DefaultHDR);
+                    
+                    Graphics.Blit(previousTexture, blurBuffer1[i], material, 1);
+
+                    previousTexture = blurBuffer1[i];
+                }
+
+                //  Upsample and combine
+                for (int i = iterations - 2; i >= 0; i--)
+                {
+                    RenderTexture basetex = blurBuffer1[i];
+                    material.SetTexture("_BaseTex", basetex);
+
+                    blurBuffer2[i] = RenderTexture.GetTemporary(basetex.width, basetex.height, 0, RenderTextureFormat.DefaultHDR);
+
+                    Graphics.Blit(previousTexture, blurBuffer2[i], material, 2);
+                    previousTexture = blurBuffer2[i];
+                }
+
+                material.SetTexture("_BaseTex", source);
+                Graphics.Blit(previousTexture, destination, material, 3);
+
+                RenderTexture.ReleaseTemporary(prefiltered);
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    RenderTexture.ReleaseTemporary(blurBuffer1[i]);
+                    RenderTexture.ReleaseTemporary(blurBuffer2[i]);
+                }
+            }
+
+            void OnDisable ()
+            {
+                DestroyImmediate(material);
+            }
+        }
+    }
+}
