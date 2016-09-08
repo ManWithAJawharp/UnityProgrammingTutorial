c1c9da25c24620904c31a4b0982782441aa064dc
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-08 17:35:55 +0200

    Add shader speeltuin scene with teapot model and lights

---
+++ Assets/Shader speeltuin/Scripts/UserInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode lightKey;
+
+    [SerializeField]
+    private GameObject lightGroup;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	    if(Input.GetKeyUp(lightKey))
+        {
+            lightGroup.SetActive(!lightGroup.activeInHierarchy);
+        }
+	}
+}
