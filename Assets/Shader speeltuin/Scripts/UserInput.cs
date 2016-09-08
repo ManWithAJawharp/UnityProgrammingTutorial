ea7ea5c45eaf3d24f1dcdceca45f7e96dff835ed
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-08 20:10:47 +0200

    Change line endings and add turntable key to turn of animation

--- Assets/Shader speeltuin/Scripts/UserInput.cs
+++ Assets/Shader speeltuin/Scripts/UserInput.cs
@@ -1,1 +1,43 @@
-﻿using UnityEngine;  public class UserInput : MonoBehaviour {     [SerializeField]     private KeyCode lightKey;     [SerializeField]     private KeyCode sceneryKey;      [SerializeField]     private GameObject lightGroup;     [SerializeField]     private GameObject sceneryGroup;  	void Update ()     { 	    if(Input.GetKeyUp(lightKey))         {             lightGroup.SetActive(!lightGroup.activeInHierarchy);         }          if (Input.GetKeyUp(sceneryKey))         {             sceneryGroup.SetActive(!sceneryGroup.activeInHierarchy);         }     } } 
+﻿using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode lightKey;
+    [SerializeField]
+    private KeyCode sceneryKey;
+    [SerializeField]
+    private KeyCode turntableKey;
+
+    [SerializeField]
+    private GameObject lightGroup;
+    [SerializeField]
+    private GameObject sceneryGroup;
+    [SerializeField]
+    private Animation turntableAnimation;
+
+	void Update ()
+    {
+	    if(Input.GetKeyUp(lightKey))
+        {
+            lightGroup.SetActive(!lightGroup.activeInHierarchy);
+        }
+
+        if (Input.GetKeyUp(sceneryKey))
+        {
+            sceneryGroup.SetActive(!sceneryGroup.activeInHierarchy);
+        }
+
+        if (Input.GetKeyUp(turntableKey))
+        {
+            if (turntableAnimation.isPlaying)
+            {
+                turntableAnimation.Stop();
+            }
+            else
+            {
+                turntableAnimation.Play();
+            }
+        }
+    }
+}
