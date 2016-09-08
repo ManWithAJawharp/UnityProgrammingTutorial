fad39ef8c4affc82f49579a2719f48bacdb889ac
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-08 22:58:18 +0200

    Add DistortionImage Effect Shader and materials and blit them on the camera

---
+++ Assets/Shader speeltuin/Scripts/ImageEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ImageEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Material effectMaterial;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
