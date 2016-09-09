f02e8f2b9e8a699138476159455cc914051cc98e
Abe Vos <abe_vos@msn.com>
2016-09-09 10:45:42 +0200

    Add new tutorial files

---
+++ Assets/Tutorial_01/Prep/Scripts/SpherlySphere.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpherlySphere : MonoBehaviour
+{
+    //  Declare Transform array, denoted by []
+    private Transform[] children;
+
+    protected void Awake ()
+    {
+        //  Fill the array using a built-in function
+        children = GetComponentsInChildren<Transform>();
+    }
+
+    protected void Update ()
+    {
+        //  children[0] is this object
+        children[1].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
+        children[2].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
+        children[3].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
+    }
+}
---
+++ Assets/Tutorial_01/Prep/Scripts/SwerlyLerp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwerlyLerp : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 start;
+    [SerializeField]
+    private Vector3 end;
+
+    private float time = 0.0f;
+
+    protected void Update ()
+    {
+        transform.position = Lerp(start, end, time);
+
+        //  Make t loop from 0 to 1 over the course of one second
+        if (time < 1f)
+            time += Time.deltaTime;
+        else
+            time = 0f;
+    }
+
+    protected Vector3 Lerp(Vector3 a, Vector3 b, float t)
+    {
+        return (1f - t) * start + t * end   //  Standard Lerp
+            + Mathf.Sin(2 * Mathf.PI * t) * Vector3.up //  Add wave movement along y-axis
+            + 2 * Mathf.Pow(2 * t - 1, 2) * Vector3.forward;    //  Add parabolic movement along z-axis
+    }
+}
