7e75e16de564fbf09e4d0f1d8c8309e18c3b9c7e
Lotta Unicorns <lotta.andriesse@gmail.com>
2016-09-10 13:30:25 +0200

    iets met een branch

--- Assets/Tutorial_00/LottaProbeert/Scripts/PonyLoopt.cs
+++ Assets/Tutorial_00/LottaProbeert/Scripts/PonyLoopt.cs
@@ -5,6 +5,7 @@ public class PonyLoopt : MonoBehaviour {
 
     public float speedForward;
     public int heightJump;
+    public int speedJump;
     
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,34 @@ public class PonyLoopt : MonoBehaviour {
 	void Update () {
         transform.Translate(0f, 0f, speedForward * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(0f, heightJump * Time.deltaTime, -5f);
+            if (transform.position.y <= heightJump)
+            {
+                transform.Translate(0f, speedJump * Time.deltaTime, 0f);
+            }
         }
     }
+    
+    protected Vector3 Lerp(Vector3 a, Vector3 b, float t)
+    {
+        return (1f - t) * a + t * b   //  Standard Lerp
+            + Mathf.Sin(2 * Mathf.PI * t) * Vector3.up //  Add wave movement along y-axis
+            + 2 * Mathf.Pow(2 * t - 1, 2) * Vector3.forward;    //  Add parabolic movement along z-axis
+    }
+    
 
-    /*    float NaarVoren (float speedForward)
+    //while hoogte < heightJump translate y met speedJump
+
+    /*  if (someObject.transform.position.x == someX)
+        {
+             do something
+        }
+     *    
+     *    
+     *    
+     *    
+     *    float NaarVoren (float speedForward)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
--- Assets/Tutorial_01/Prep/Scripts/SpherlySphere.cs
+++ Assets/Tutorial_01/Prep/Scripts/SpherlySphere.cs
@@ -5,9 +5,10 @@ public class SpherlySphere : MonoBehaviour
 {
     //  Declare Transform array, denoted by []
     private Transform[] children;
-
+    public int experimenteren;
     protected void Awake ()
     {
+
         //  Fill the array using a built-in function
         children = GetComponentsInChildren<Transform>();
     }
@@ -15,8 +16,10 @@ public class SpherlySphere : MonoBehaviour
     protected void Update ()
     {
         //  children[0] is this object
-        children[1].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
-        children[2].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
-        children[3].position += Time.deltaTime * Mathf.Sin(Time.time) * Vector3.up;
+        children[1].position += Time.deltaTime * Mathf.Sin(Time.time) * new Vector3(-1f, 0.5f, 0.5f);
+        for (int i = 2; i < children.Length; i++)
+        {
+            children[i].position += Time.deltaTime * Mathf.Sin(Time.time + i * experimenteren) * new Vector3(transform.position.x + Mathf.Sin(i*2), transform.position.x * Mathf.Sin(i*3), transform.position.x - Mathf.Sin(i));
+        }
     }
 }
