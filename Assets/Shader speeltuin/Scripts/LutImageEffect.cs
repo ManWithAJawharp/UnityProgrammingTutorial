f80855f77acf602373762f5e493c080d02bc4a67
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-11 13:34:55 +0200

    Add lut shader and inspector script

---
+++ Assets/Shader speeltuin/Scripts/LutImageEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LutImageEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Texture lutTexture;
+    private Material effectMaterial;
+
+    void Start()
+    {
+        effectMaterial = new Material(Shader.Find("Hidden/LutImageEffectShader"));
+        effectMaterial.SetTexture("_MainTex", lutTexture);
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
