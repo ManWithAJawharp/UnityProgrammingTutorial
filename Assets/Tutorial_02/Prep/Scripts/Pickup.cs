6a9ac2eaf9519d80aa6e229fe773b6e91348b657
Abe Vos <abe_vos@msn.com>
2016-09-14 10:17:36 +0200

    Tutorial 2

--- Assets/Tutorial_02/Prep/Scripts/Pickup.cs
+++ Assets/Tutorial_02/Prep/Scripts/Pickup.cs
@@ -1,42 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pickup : MonoBehaviour
+namespace Tutorial_02
 {
-	private enum State
-	{
-		Idle, Excited, Action
-	}
+    public class Pickup : MonoBehaviour
+    {
+        private enum State
+        {
+            Idle, Excited, Action
+        }
 
-	private Transform avatar;
-	private State currentState;
+        private Transform avatar;
+        private State currentState;
+        private float state_t;
 
-	protected void Awake ()
-	{
-		avatar = transform.GetChild (0);
-		currentState = State.Excited;
-	}
+        protected void Awake()
+        {
+            avatar = transform.GetChild(0);
+            currentState = State.Idle;
+            state_t = 0.0f;
+        }
 
-	protected void Update ()
-	{
-		switch (currentState)
-		{
-		case State.Idle:
-			avatar.position = 0.3f * Mathf.Sin(Time.time) * Vector3.up;
-			avatar.eulerAngles = 20.0f * Time.time * Vector3.up;
-			break;
-		case State.Excited:
-			avatar.position = 0.3f * Mathf.Abs (Mathf.Sin (4f * Time.time)) * Vector3.up;
-			avatar.eulerAngles = 80.0f * Time.time * Vector3.up;
-			break;
-		case State.Action:
-			break;
-		}
-	}
+        protected void Update()
+        {
+            switch (currentState)
+            {
+                case State.Idle:
+                    avatar.position = 0.3f * Mathf.Sin(Time.time) * Vector3.up;
+                    avatar.eulerAngles = 20.0f * Time.time * Vector3.up;
+                    break;
+                case State.Excited:
+                    avatar.position = 0.3f * Mathf.Abs(Mathf.Sin(4f * Time.time)) * Vector3.up;
+                    avatar.eulerAngles = 80.0f * Time.time * Vector3.up;
 
-	private void SetState(State newState)
-	{
-		State oldState = currentState;
-		currentState = newState;
-	}
+                    break;
+                case State.Action:
+                    avatar.position = 2.0f * state_t * Vector3.up;
+                    avatar.eulerAngles = 60.0f * Time.time * Vector3.up;
+                    avatar.localScale = 5f * (0.2f - state_t) * Vector3.one;
+
+                    if (state_t > 0.2f)
+                    {
+                        Destroy(gameObject);
+                    }
+
+                    break;
+            }
+
+            state_t += Time.deltaTime;
+        }
+
+        protected void OnTriggerEnter(Collider col)
+        {
+            if (col.tag == "Player")
+            {
+                col.GetComponent<Player>().Pickup();
+                SetState(State.Action);
+            }
+        }
+
+        private void SetState(State newState)
+        {
+            State oldState = currentState;
+            currentState = newState;
+            state_t = 0.0f;
+        }
+    }
 }
---
+++ Assets/Tutorial_02/Prep/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tutorial_02
+{
+    public class Player : MonoBehaviour
+    {
+        public void Pickup()
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
