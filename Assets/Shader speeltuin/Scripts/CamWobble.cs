52f44949fe34cb9a6181b9b5c852e8e446e6afa9
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-16 23:38:29 +0200

    Add camera wobble and fov distortion based on lissajous figure

---
+++ Assets/Shader speeltuin/Scripts/CamWobble.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CamWobble : MonoBehaviour
+{
+    [SerializeField, Range(0, 3)]
+    private float speed;
+    [SerializeField, Range(0,5)]
+    private float LissajousScale;
+    [SerializeField, Range(0, 10)]
+    private float fovScale;
+    private float waveTime;
+    private Vector3 startPosition;
+    private float startFov;
+    private Camera cam;
+
+    // Use this for initialization
+    void Awake()
+    {
+        startPosition = transform.position;
+        waveTime = 0;
+        cam = GetComponent<Camera>();
+        startFov = cam.fieldOfView;
+    }
+
+    void Update()
+    {
+        waveTime += Time.deltaTime * speed;
+        Vector2 coords = MakeLissajousCoords(waveTime, LissajousScale);
+        transform.position = new Vector3(coords.x + startPosition.x , startPosition.y, coords.y + startPosition.z);
+        transform.LookAt(Vector3.zero);
+        cam.fieldOfView = fovScale * -coords.x + startFov;
+        //cam.fieldOfView = fovScale * Vector3.Distance(transform.position-startPosition,Vector3.zero) + startFov;
+    }
+
+    Vector2 MakeLissajousCoords(float time,float scale)
+    {
+        // pi/2 = 1.57079632679
+        Vector2 coords = new Vector2();
+        coords.x = scale * Mathf.Sin(5f * time + 1.57079632679f);
+        coords.y = scale * Mathf.Sin(4f * time);
+        return coords;
+    }
+}
