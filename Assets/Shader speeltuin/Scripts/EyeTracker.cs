f859622cd4e2607e6b904beab2b64c39e2bde8c8
Mathijs Lemmers <mathijs750@gmail.com>
2016-09-18 13:23:34 +0200

    Add eye tracking script to all eyes an tweak the parent child relations to make the tracking work

---
+++ Assets/Shader speeltuin/Scripts/EyeTracker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EyeTracker : MonoBehaviour
+{
+	void Update ()
+    {
+        Quaternion lookatRotation = Quaternion.LookRotation(Camera.main.transform.position);
+        //lookatRotation.eulerAngles += new Vector3(0, -90, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookatRotation, Time.deltaTime*4);
+	}
+}
