559bc83750ba383dfb32ea5ed0cec4266d8c7574
Abe Vos <abe_vos@msn.com>
2016-09-20 15:38:38 +0200

    Implement native library

--- Assets/Tutorial_02/Prep/Scripts/Player.cs
+++ Assets/Tutorial_02/Prep/Scripts/Player.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 namespace Tutorial_02
 {
-    public class Player : MonoBehaviour
+	public class Player : MonoBehaviour
     {
+		public delegate void StateDelegate(int newState, int oldState);
+		public static event StateDelegate onStateChanged;
+
         public void Pickup()
         {
             GetComponent<Renderer>().material.color = Color.red;
--- Assets/plugin_test/scripts/plugin_test.cs
+++ Assets/plugin_test/scripts/plugin_test.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
 
-public class plugin_test : MonoBehaviour {
+public class plugin_test : MonoBehaviour
+{
+	[DllImport ("libcpp_library_test")]
+	private static extern void Sort(int[] a, int length);
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		int[] a = new int[] {5, 1, 20, 4, 8, 19};
+		Sort (a, a.Length);
+		for (int i = 0; i < a.Length; i++)
+		{
+			Debug.Log(a [i]);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
